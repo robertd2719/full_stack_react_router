e08ad3b937b71de53c2e61a65e32452610ca78b5
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2022-01-04 04:45:01 -0500

    timer output change

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import useTimer from '../hooks/useTimer';
+import React from "react";
+import { Link } from "react-router-dom";
+import useTimer from "../hooks/useTimer";
 
 export default function NavBar() {
-    const timer = useTimer(new Date());
-    return (
-        <nav>
-            <span className="nav__logo">LOGO</span>
-            <div className="nav__links">
-                <Link to="/add">Add</Link>
-                <Link to="/about">About</Link>
-                <Link to="/">Home</Link>
-            </div>
-                <h4>{timer.getHours()}:{timer.getMinutes()}:{timer.getSeconds()}</h4>
-        </nav>
-    )
+  const timer = useTimer(new Date());
+  return (
+    <nav>
+      <span className="nav__logo">LOGO</span>
+      <div className="nav__links">
+        <Link to="/add">Add</Link>
+        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <h4>
+        {timer.getHours()}:{timer.getMinutes()}:{timer.getSeconds()<10 && "0" }
+        {timer.getSeconds()}
+      </h4>
+    </nav>
+  );
 }
